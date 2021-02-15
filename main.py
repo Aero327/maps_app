2826df1bb11e4311035365e75cf186d14a80f729
MOVS Laboratory <lab.movs@outlook.com>
2021-02-15 19:10:13 +0500

    №4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 
 def get_map():
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={COORDS}&z={SCALE}&l=map"
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={COORDS}&z={SCALE}&l={cur_spn}"
     response = requests.get(map_request)
 
     if not response:
@@ -23,6 +23,7 @@ def get_map():
 
 SCALE = 13
 COORDS = '56.229421,58.022833'
+cur_spn = 'map'
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
@@ -48,6 +49,16 @@ while True:
                 if 8 < SCALE + 1:
                     SCALE -= 1
                     map_request, response, map_file = get_map()
+
+            if event.key == pygame.K_SPACE:
+                if cur_spn == 'map':
+                    cur_spn = 'sat'
+                elif cur_spn == 'sat':
+                    cur_spn = 'sat,skl'
+                else:
+                    cur_spn = 'map'
+
+                map_request, response, map_file = get_map()
 
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
